c9c839be5dfe337972c1abb3e43dbc93d9c8d7c0
Max Brodeur-Urbas <murbas98@gmail.com>
2019-03-02 18:32:01 -0500

    Student Unit tests added

---
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/CooperatorStudentTest.java
@@ -0,0 +1,118 @@
+package ca.mcgill.ecse321.cooperator;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ca.mcgill.ecse321.cooperator.controller.CooperatorController;
+import ca.mcgill.ecse321.cooperator.dao.*;
+import ca.mcgill.ecse321.cooperator.model.*;
+import ca.mcgill.ecse321.cooperator.services.CoursesService;
+import ca.mcgill.ecse321.cooperator.services.StudentService;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CooperatorStudentTest{
+
+	@Mock
+	private StudentRepository studentDao;
+	
+	@InjectMocks
+	private StudentService studentService;
+
+	private Student student;
+	private static final Integer STUDENT_KEY = 10;
+	private static final Integer WRONG_STUDENT_KEY = 69;
+	private static final Boolean STUDENT_STATUS = true;
+	private static final Boolean NEW_STUDENT_STATUS = false;
+	
+	private List<Student> expectedList = new ArrayList<Student>();
+	
+	
+	@Before
+	public void setMockOutput() {
+		when(studentDao.findById(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
+			if (invocation.getArgument(0).equals(STUDENT_KEY)) {
+				Student student = new Student();
+				student.setStudentID(STUDENT_KEY);
+				student.setProblematic(STUDENT_STATUS);
+				return student;
+			} else {
+				return null;
+			}
+		
+		});
+		when(studentDao.findStudentByProblematic(STUDENT_STATUS)).thenAnswer((InvocationOnMock invocation) ->{
+			List<Student> list = new ArrayList<Student>();
+			list.add(student);
+			return list;
+			
+		});
+	}
+
+	
+	@Before
+	public void setUpMocks() {
+		student = mock(Student.class);
+		student = studentService.createStudent();
+		expectedList.add(student);
+	}
+	
+	@Test
+	public void testStudentCreation() {
+		assertNotNull(student);
+	}
+	
+	@Test
+	public void testStudentQueryFound() {
+		assertEquals(STUDENT_KEY, studentService.getStudentById(STUDENT_KEY).getStudentID());
+		assertEquals(STUDENT_STATUS,studentService.getStudentById(STUDENT_KEY).getProblematic());
+		assertNull(studentService.getStudentById(STUDENT_KEY).getFirstName());
+		assertNull(studentService.getStudentById(STUDENT_KEY).getLastName());
+		assertNull(studentService.getStudentById(STUDENT_KEY).getCoopPosition());
+
+	}
+	
+	@Test
+	public void testStudentQueryNotFound() {
+		assertNull(studentService.getStudentById(WRONG_STUDENT_KEY));
+	}
+	
+	@Test
+	public void testGetProblematicStudents() {
+		assertEquals(expectedList, studentService.getAllProblematicStudents());
+	}
+	
+	@Test
+	public void testSetProblematicStatus() {
+		Student s = studentService.getStudentById(STUDENT_KEY);
+		s.setProblematic(NEW_STUDENT_STATUS);
+		assertEquals(NEW_STUDENT_STATUS, s.getProblematic());
+	}
+	
+	
+	
+	
+	
+	
+}
