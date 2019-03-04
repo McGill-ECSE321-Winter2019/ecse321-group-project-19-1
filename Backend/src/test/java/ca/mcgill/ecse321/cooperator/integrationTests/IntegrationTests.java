46d4aa6e47a6cb68c011f72b14c5716d2a57f334
Mia Zhou <yuxin.zhou2@mail.mcgill.ca>
2019-03-04 14:03:55 -0500

    SendRequests class

--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/integrationTests/IntegrationTests.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/integrationTests/IntegrationTests.java
@@ -3,10 +3,18 @@ package ca.mcgill.ecse321.cooperator.integrationTests;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import ca.mcgill.ecse321.cooperator.dao.CoopPositionRepository;
+import ca.mcgill.ecse321.cooperator.dao.CourseRepository;
+import ca.mcgill.ecse321.cooperator.dao.EmployerRepository;
+import ca.mcgill.ecse321.cooperator.dao.RequiredDocumentRepository;
+import ca.mcgill.ecse321.cooperator.dao.StudentRepository;
+import ca.mcgill.ecse321.cooperator.dao.UserEntityRepository;
 import ca.mcgill.ecse321.cooperator.model.CoopPosition;
 import ca.mcgill.ecse321.cooperator.model.RequiredDocument;
 import ca.mcgill.ecse321.cooperator.model.Student;
@@ -25,6 +33,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class IntegrationTests {
 
@@ -33,94 +42,63 @@ public class IntegrationTests {
 	private final String EMAIL = "yoyo@gmail.com";
 	private JSONObject joResponse;
 	private JSONArray jaResponse;
+	
+	@Autowired
+	private RequiredDocumentRepository rdocRepo;
+	@Autowired
+	private CourseRepository courseRepo;
+	@Autowired
+	private UserEntityRepository userRepo;
+	@Autowired
+	private CoopPositionRepository cpRepo;
+	@Autowired
+	private StudentRepository studentRepo;
+	@Autowired
+	private EmployerRepository employerRepo;
+	
+	
+	
+//	@After
+//	public void clearDatabase() {
+//		rdocRepo.deleteAll();
+//		employerRepo.deleteAll();
+//		courseRepo.deleteAll();
+//		userRepo.deleteAll();
+//		cpRepo.deleteAll();
+//		studentRepo.deleteAll();
+//	}
 
-	private JSONObject sendRequest(String requestType, String baseUrl, String path, String parameters) {
-		try {
-			URL url = new URL(baseUrl + path + ((parameters == null) ? "" : ("?" + parameters)));
-			System.out.println("Sending: " + url.toString());
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod(requestType);
-			connection.setRequestProperty("Accept", "application/json");
-			if (connection.getResponseCode() != 200) {
-				throw new RuntimeException(
-						url.toString() + " failed : HTTP error code : " + connection.getResponseCode());
-			}
-			JSONObject r = new JSONObject(
-					new BufferedReader(new InputStreamReader((connection.getInputStream()))).readLine());
-			assertEquals(200, connection.getResponseCode());
-			connection.disconnect();
-			return r;
-		} catch (JSONException | IOException e) {
-			e.printStackTrace();
-		} catch (RuntimeException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	private JSONObject sendRequest(String requestType, String baseUrl, String path) {
-		return sendRequest(requestType, baseUrl, path, null);
-	}
-
-	private JSONArray sendRequestArray(String requestType, String baseUrl, String path, String parameters) {
-		try {
-			URL url = new URL(baseUrl + path + ((parameters == null) ? "" : ("?" + parameters)));
-			System.out.println("Sending: " + url.toString());
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod(requestType);
-			connection.setRequestProperty("Accept", "application/json");
-			if (connection.getResponseCode() != 200) {
-				throw new RuntimeException(
-						url.toString() + " failed : HTTP error code : " + connection.getResponseCode());
-			}
-			JSONArray r = new JSONArray(
-					new BufferedReader(new InputStreamReader((connection.getInputStream()))).readLine());
-			assertEquals(200, connection.getResponseCode());
-			connection.disconnect();
-			return r;
-		} catch (JSONException | IOException e) {
-			e.printStackTrace();
-		} catch (RuntimeException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	private JSONArray sendRequestArray(String requestType, String baseUrl, String path) {
-		return sendRequestArray(requestType, baseUrl, path, null);
-	}
-
-	@Test
-	public void TestAddingCourse() {
-		try {
-			joResponse = sendRequest("POST", BASE_URL, "/createCourse", "courseName=" + COURSE_NAME);
-			System.out.println("Received: " + joResponse.toString());
-			assertEquals(COURSE_NAME, joResponse.getString("courseName"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
+//	@Test
+//	public void TestAddingCourse() {
+//		try {
+//			joResponse = SendRequests.sendRequest("POST", BASE_URL, "/createCourse", "courseName=" + COURSE_NAME);
+//			System.out.println("Received: " + joResponse.toString());
+//			assertEquals(COURSE_NAME, joResponse.getString("courseName"));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
 //	@Test
 //	public void testGetProblematic() {
 //		try {
 //
 //			// creating two students
-//			JSONObject student1 = sendRequest("POST", BASE_URL, "/createStudent",
+//			JSONObject student1 = SendRequests.sendRequest("POST", BASE_URL, "/createStudent",
 //					"firstName=" + "max" + "&lastName=" + "brodeur");
-//			sendRequest("POST", BASE_URL, "/createStudent", "firstName=" + "andre" + "&lastName=" + "kaba");
+//			SendRequests.sendRequest("POST", BASE_URL, "/createStudent", "firstName=" + "andre" + "&lastName=" + "kaba");
 //			int STUDENT_ID = student1.getInt("studentId");
 //
 //			// creating coop position for student1
-//			JSONObject cp = sendRequest("POST", BASE_URL, "/createCoop", "startDate=02/01/2018" + "&endDate=02/01/2019"
+//			JSONObject cp = SendRequests.sendRequest("POST", BASE_URL, "/createCoop", "startDate=02/01/2018" + "&endDate=02/01/2019"
 //					+ "&description=hello" + "&location=montreal" + "&term=fall" + "&studentId=" + STUDENT_ID);
 //			System.out.println("Received: " + cp.toString());
 //
 //			// getting problematic student. expected output to be student2
-//			jaResponse = sendRequestArray("GET", BASE_URL, "/problematicStudents");
+//			jaResponse = SendRequests.sendRequestArray("GET", BASE_URL, "/problematicStudents");
+//			System.out.println("Received: " + jaResponse.toString());
 //			assertEquals("andre", jaResponse.getJSONObject(1).get("firstName"));
+//			assertNotEquals("max", jaResponse.getJSONObject(0).get("firstName"));
 //
 //		} catch (Exception e) {
 //			e.printStackTrace();
@@ -130,10 +108,10 @@ public class IntegrationTests {
 //	@Test
 //	public void testGradeDocument() {
 //		try {
-//			JSONObject doc=sendRequest("POST", BASE_URL, "/createForm", "name=hello" + "&dueDate=02/01/2019" + "&coopId=19");
+//			JSONObject doc=SendRequests.sendRequest("POST", BASE_URL, "/createForm", "name=hello" + "&dueDate=02/01/2019" + "&coopId=19");
 //			int doc_id = doc.getInt("documentId");
 //			System.out.println("DEBUG: " + doc.toString());
-//			joResponse = sendRequest("POST", BASE_URL, "/gradeDocument",
+//			joResponse = SendRequests.sendRequest("POST", BASE_URL, "/gradeDocument",
 //					"documentId=" + doc_id + "grade=true" + "instructorEmail=" + EMAIL);
 //			System.out.println("Received: " + joResponse.toString());
 //			assertEquals(true, joResponse.getString("grade"));
@@ -142,7 +120,7 @@ public class IntegrationTests {
 //		}
 //
 //	}
-	
+//	
 //	@Test
 //	public void testSubmitDocument() {
 //		//TODO 
@@ -153,7 +131,7 @@ public class IntegrationTests {
 //	@Test
 //	public void testViewGrade() {
 //		try {
-//			joResponse=sendRequest("GET", BASE_URL, "/grade", "documentId=19");
+//			joResponse=SendRequests.sendRequest("GET", BASE_URL, "/grade", "documentId=19");
 //			System.out.println("Received: " + joResponse.toString());
 //			assertEquals("COMPLETED", joResponse.getString("status"));
 //		} catch (Exception e) {
@@ -161,11 +139,11 @@ public class IntegrationTests {
 //		}
 //		
 //	}
-	
+//	
 //	@Test
 //	public void testAdjudicateCoop() {
 //		try {
-//			joResponse=sendRequest("POST", BASE_URL, "/setCoopStatus", "coopId=19" +"&status=COMPLETED");
+//			joResponse=SendRequests.sendRequest("POST", BASE_URL, "/setCoopStatus", "coopId=19" +"&status=COMPLETED");
 //			System.out.println("Received: " + joResponse.toString());
 //			assertEquals("COMPLETED", joResponse.getString("status"));
 //		} catch (Exception e) {
