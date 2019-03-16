38fed9d3959e40faaf9e8e435caa70b7627d7f14
Andre Kaba <andree.kaba@mail.mcgill.ca>
2019-03-16 18:01:55 -0400

    Fix #100 and add a basic website

---
+++ Frontend/cooperator-web/src/components/Cooperator.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="cooperator">
+    <h2>Dummy page</h2>
+    <table>
+      <tr>
+        <td>Yo dawg</td>
+        <td>Wassap?</td>
+      </tr>
+      <tr>
+        <td>
+          <input type="text" placeholder="Person Name">
+        </td>
+        <td>
+          <button>Click Me!</button>
+        </td>
+      </tr>
+    </table>
+    <p>
+      <span style="color:red">Errorz</span>
+    </p>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'cooperator',
+    data () {
+      return {
+        people: [],
+        newPerson: '',
+        errorPerson: '',
+        response: []
+      }
+    },
+  }
+</script>
+<style>
+  #cooperator {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
