5a29cdfdefa82411bf014bbb4ea3e0e837dcf666
Mia Zhou <yuxin.zhou2@mail.mcgill.ca>
2019-03-18 17:40:14 -0400

    #92: fixes

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/services/UserEntityService.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/services/UserEntityService.java
@@ -104,17 +104,6 @@ public class UserEntityService {
     	if(ue == null) {
     		throw new NullPointerException("No such user.");
     	}
-    	if(ue instanceof TermInstructor) {
-    		Set<CoopPosition> cps = ((TermInstructor) ue).getCoopPosition();
-    		if(cps.size() > 0) {
-    			for(CoopPosition cp : cps) {
-    				//if coop position is on going, then cannot delete term instructor
-    				if(cp.getEndDate().after(new Date())) { 
-    					throw new IllegalStateException("Cannot delete term instructor with on going coop positions.");
-    				}
-    			}
-    		}
-    	}
     	userEntityRepository.deleteById(email);	
     	return true;
     	
--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/CooperatorUserEntityTest.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/CooperatorUserEntityTest.java
@@ -20,10 +20,8 @@ import org.mockito.invocation.InvocationOnMock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -69,32 +67,16 @@ public class CooperatorUserEntityTest {
 	private static final String password = "Not My Password";
 	private static final String WRONG_EMAIL = "Wrong@Email.com";
 
-	private TermInstructor ti2;
-	private static final String email3 = "IamJobs@gmail.com";
-	private static final String firstName3 = "Jef";
-	private static final String lastName3 = "Ok";
-
 	private CoopPosition cp1;
 	private static final Integer COOP_KEY = 10;
 	private static final Date startDate = new Date(5);
 	private static final Date endDate = new Date(10);
-
-	private CoopPosition cp2;
-	private static final Integer COOP_KEY2 = 10;
-	private static final Date startDate2 = new Date(5);
 	
-	
-	private Student student_past;
+	private Student student;
 	private static final Integer STUDENT_KEY1 = 10;
-
-	private Student student_present;
-	private static final Integer STUDENT_KEY2 = 11;
-	
-
 
 	private List<UserEntity> expectedList = new ArrayList<UserEntity>();
 	private Set<CoopPosition> cps_ti1 = new HashSet<>();
-	private Set<CoopPosition> cps_ti2 = new HashSet<>();
 
 	@Before
 	public void setMockOutput() {
@@ -113,13 +95,6 @@ public class CooperatorUserEntityTest {
 				user.setEmail(email2);
 				user.setPassword(password);
 				return user;
-			} else if (invocation.getArgument(0).equals(email3)) {
-				UserEntity user = new TermInstructor();
-				user.setFirstName(firstName3);
-				user.setLastName(lastName3);
-				user.setEmail(email3);
-				user.setPassword(password);
-				return user;
 			} else {
 				return null;
 			}
@@ -130,10 +105,6 @@ public class CooperatorUserEntityTest {
 			if (invocation.getArgument(0).equals(STUDENT_KEY1)) {
 				Student student = new Student();
 				student.setStudentID(STUDENT_KEY1);
-				return student;
-			} else if (invocation.getArgument(0).equals(STUDENT_KEY2)) {
-				Student student = new Student();
-				student.setStudentID(STUDENT_KEY2);
 				return student;
 			} else {
 				return null;
@@ -147,12 +118,7 @@ public class CooperatorUserEntityTest {
 				cp.setCoopId(COOP_KEY);
 				return cp;
 
-			} else if (invocation.getArgument(0).equals(COOP_KEY2)) {
-				CoopPosition cp = new CoopPosition();
-				cp.setCoopId(COOP_KEY2);
-				return cp;
-
-			} else {
+			}else {
 				return null;
 			}
 		});
@@ -170,50 +136,33 @@ public class CooperatorUserEntityTest {
 		ti1 = userEntityService.createTermInstructor(firstName2, lastName2, email2, password);
 		expectedList.add(ti1);
 
-		// term instructor with on going coop
-		ti2 = mock(TermInstructor.class);
-		ti2 = userEntityService.createTermInstructor(firstName3, lastName3, email3, password);
-		expectedList.add(ti2);
 
 		// student that has no on going coop
-		student_past = mock(Student.class);
-		student_past = studentService.createStudent(firstName, lastName);
+		student = mock(Student.class);
+		student = studentService.createStudent(firstName, lastName);
 
-		// student that has on going coop
-		student_present = mock(Student.class);
-		student_present = studentService.createStudent(firstName, lastName);
 
 		// coop 1 is a past coop position
 		cp1 = mock(CoopPosition.class);
-		cp1 = coopPositionService.createCoopPosition(startDate, endDate, "asdfas", "dafsd", "fdasfa", student_past);
+		cp1 = coopPositionService.createCoopPosition(startDate, endDate, "asdfas", "dafsd", "fdasfa", student);
 		cps_ti1.add(cp1);
-
-		// coop 2 is an on going coop position
-		Date currentDate= new Date();
-		LocalDateTime localDateTime = currentDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-		localDateTime = localDateTime.plusYears(1).plusMonths(1).plusDays(1);
-		Date endDate2 = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-		cp2 = mock(CoopPosition.class);
-		cp2 = coopPositionService.createCoopPosition(startDate2, endDate2, "asdfad", "asdfas", "asdfadsf", student_present);
-		cps_ti2.add(cp2);
 
 		// assigning coop set to the term instructor
 		userEntityService.assignCoopToInstructor(ti1, cps_ti1);
-		userEntityService.assignCoopToInstructor(ti2, cps_ti2);
+		
 	}
 
 	@Test
 	public void testUserEntityCreation() {
 		assertNotNull(pm);
 		assertNotNull(ti1);
-		assertNotNull(ti2);
+		
 	}
 
 	@Test
 	public void testUserEntityQueryFound() {
 		assertEquals(email, userEntityService.getUserEntityByEmail(email).getEmail());
 		assertEquals(email2, userEntityService.getUserEntityByEmail(email2).getEmail());
-		assertEquals(email3, userEntityService.getUserEntityByEmail(email3).getEmail());
 	}
 
 	@Test
@@ -226,15 +175,15 @@ public class CooperatorUserEntityTest {
 		// deleting program manager
 		assertEquals(true, userEntityService.deleteUserEntity(email));
 
-		// deleting term instructor that has past coop
+		// deleting term instructor
 		assertEquals(true, userEntityService.deleteUserEntity(email2));
 
 	}
+	
+	@Test (expected=NullPointerException.class)
+	public void testNullUserEntityDeletion() {
+		userEntityService.deleteUserEntity(WRONG_EMAIL);
+	}
 
-//	@Test(expected = IllegalStateException.class)
-//	public void testTermInstructorIllegalDeletion() {
-//		// deleting term instructor that has on going coop
-//		userEntityService.deleteUserEntity(email3);
-//	}
 
 }
