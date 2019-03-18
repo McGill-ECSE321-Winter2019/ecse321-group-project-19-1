2a4f11c39349d2ba57f9bf68a6bdaaaf6c281b08
Sophie Deng <sophie_deng97@hotmail.com>
2019-03-18 18:06:28 -0400

    add login page

---
+++ Frontend/cooperator-web/src/components/Login.vue
@@ -0,0 +1,64 @@
+<template>
+<div id="cooperator">
+  <div class="site-wrap" style="height:100%">
+      <div class="site-mobile-menu">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+      <div class="half left">
+        <div class="site-section site-hero inner">
+          <div class="container" align="center">
+            <div class="row align-items-center" align="center">
+              <div class="col-md-10" align="center" style="padding-left:10%">
+                <span class="d-block mb-3 caption">ECSE 321</span>
+                <h1 class="d-block mb-4">Coop-erator</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="half right">
+        <div class="site-section site-hero inner">
+          <div class="container" align="center">
+            <div class="login-grid" style="padding-right:10%">
+              <input class="login-text" type="text" placeholder="Username" />
+              <input
+                class="login-text"
+                type="password"
+                placeholder="Password"
+              />
+              <input
+                type="submit"
+                value="Login"
+                class="btn btn-primary py-2 px-4 text-white"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      <footer class="site-footer">
+        <div class="container">
+            <div class="col-md-12 text-center">
+              <div class="border-top pt-5">
+                <p>
+                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                  Copyright &copy;
+                  All rights reserved | This template is made with
+                  <i class="icon-heart text-primary" aria-hidden="true"></i> by
+                  <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+              </div>
+          </div>
+        </div>
+      </footer>
+</div>
+</template>
--- Frontend/cooperator-web/src/router/index.js
+++ Frontend/cooperator-web/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Cooperator from '@/components/Cooperator'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/app',
       name: 'Cooperator',
       component: Cooperator
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
