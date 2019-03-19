f96d52e72a90e4792223522c91f56ac41ca7aff7
Sophie Deng <sophie_deng97@hotmail.com>
2019-03-19 12:28:34 -0400

    #115 fix login in footer

--- Frontend/cooperator-web/src/App.vue
+++ Frontend/cooperator-web/src/App.vue
@@ -1,12 +1,43 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+<div id="app">
+    <div class="site-wrap" style="height:100%">
+        <div class="site-mobile-menu">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+            </div>
+            <div class="site-mobile-menu-body">
+            </div>
+        </div>
+
+        <router-view></router-view>
+
+        <footer class="site-footer">
+            <div class="container">
+
+                <div class="text-center">
+                    <div class="border-top pt-5">
+                        <p>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;
+                            All rights reserved | This template is made with
+                            <i class="icon-heart text-primary" aria-hidden="true"></i> by
+                            <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            | @Authors BRODEUR Max, DENG Sophie, ELKHOURY Carl, KABA Andre, ZHOU Mia
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+    name: 'app'
 }
 </script>
 
--- Frontend/cooperator-web/src/components/Login.vue
+++ Frontend/cooperator-web/src/components/Login.vue
@@ -1,73 +1,42 @@
 <template>
 <div id="login">
-    <div class="site-wrap" style="height:100%">
-        <div class="site-mobile-menu">
-            <div class="site-mobile-menu-header">
-                <div class="site-mobile-menu-close mt-3">
-                    <span class="icon-close2 js-menu-toggle"></span>
-                </div>
-            </div>
-            <div class="site-mobile-menu-body"></div>
-        </div>
 
+    <div class="site-section site-hero inner">
         <div class="half left">
-            <div class="site-section site-hero inner">
-                <div class="container" align="center">
-                    <div class="row align-items-center" align="center">
-                        <div class="col-md-10" align="center" style="padding-left:10%">
-                            <span class="d-block mb-3 caption">ECSE 321</span>
-                            <h1 class="d-block mb-4">Coop-erator</h1>
-                        </div>
+            <div class="container" align="center">
+                <div class="row align-items-center" align="center">
+                    <div class="col-md-10" align="center" style="padding-left:10%">
+                        <span class="d-block mb-3 caption">ECSE 321</span>
+                        <h1 class="d-block mb-4">Coop-erator</h1>
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="half right">
-                <div class="site-section site-hero inner">
-                    <div class="container" align="center">
-                        <div class="login-grid" style="padding-right:10%">
-                            <input
+            <div class="container" align="center">
+                <div class="login-grid" style="padding-right:10%">
+                    <input
                         class="login-text"
                         type="text"
                         placeholder="Username"
                         v-model="username"
                         />
-                            <input 
-                class="login-text"
-                type="password"
-                placeholder="Password"
-                v-model="password"
-              />
-                            <input @click="login(username, password)"
-                type="submit"
-                value="Login"
-                class="btn btn-primary py-2 px-4 text-white"
-              />
-            </div>
-                        </div>
+                    <input
+                        class="login-text"
+                        type="password"
+                        placeholder="Password"
+                        v-model="password"
+                        />
+                    <input @click="login(username, password)"
+                        type="submit"
+                        value="Login"
+                        class="btn btn-primary py-2 px-4 text-white"
+                        />
                     </div>
                 </div>
+            </div>
         </div>
-
-        <footer class="site-footer">
-            <div class="container">
-
-                <div class="text-center">
-                    <div class="border-top pt-5">
-                        <p>
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;
-                            All rights reserved | This template is made with
-                            <i class="icon-heart text-primary" aria-hidden="true"></i> by
-                            <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </p>
-                    </div>
-
-                </div>
-            </div>
-        </footer>
     </div>
 </template>
 
