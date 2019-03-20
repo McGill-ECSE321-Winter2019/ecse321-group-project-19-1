2dc435ed452d9db22fe09f2f3a1b32b19d8450d3
carl-elkhoury <carlelkhoury@hotmail.com>
2019-03-20 12:33:38 -0400

    Added the tiStudent vue

---
+++ Frontend/cooperator-web/src/components/javascript/tiStudent.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+var config = require('../../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+    baseURL: backendUrl,
+    headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+export default {
+    name: 'tiStudent',
+    data() {
+        return {
+
+        }
+    },
+    created: function () {
+    
+    },
+    methods: {
+        
+    }
+}
