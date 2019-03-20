b5614911d10474eba25bc1813336eb02b5f2806a
carl-elkhoury <carlelkhoury@hotmail.com>
2019-03-20 12:33:38 -0400

    Added the tiStudent vue

---
+++ Frontend/cooperator-web/src/components/tiStudent.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="id">
+
+    <div class="site-section site-hero inner">
+        <div class="half left">
+            <div class="container" align="center">
+                <div class="row align-items-center" align="center">
+                    <div class="col-md-10" align="center" style="padding-left:10%">
+                        <span class="d-block mb-3 caption">ECSE 321</span>
+                        <h1 class="d-block mb-4">TI Student</h1>
+                        <div id="table">
+                            <b-table class="info-table"  striped hover :items= "coops" :fields= "fields"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+</template>
