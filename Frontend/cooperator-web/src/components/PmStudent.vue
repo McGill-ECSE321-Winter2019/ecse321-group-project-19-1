8f9b585db56e56c730e27f48fcff32b0a981e658
Max Brodeur-Urbas <murbas98@gmail.com>
2019-03-20 17:42:24 -0400

    Fixed status refreshing in table

--- Frontend/cooperator-web/src/components/PmStudent.vue
+++ Frontend/cooperator-web/src/components/PmStudent.vue
@@ -5,8 +5,8 @@
             <div class="site-section" style="padding-top:10%">
                 <div class="container" style="align:center">
                     <h1 class="page-title">Program Manager Students</h1>
-                    <div id="table">
-                            <b-table class="info-table"  striped hover :items= "students" :fields= "fields"/>
+                    <div id="table" >
+                            <b-table  ref="table" class="info-table"  striped hover :items= "students" :fields= "fields"/>
                      </div>
                 </div>
             </div>
