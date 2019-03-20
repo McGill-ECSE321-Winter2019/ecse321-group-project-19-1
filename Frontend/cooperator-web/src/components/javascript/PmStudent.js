a9d038f8b0c41139234fb18a4942912e6748bb2a
Max Brodeur-Urbas <murbas98@gmail.com>
2019-03-20 17:42:24 -0400

    Fixed status refreshing in table

--- Frontend/cooperator-web/src/components/javascript/PmStudent.js
+++ Frontend/cooperator-web/src/components/javascript/PmStudent.js
@@ -1,58 +1,67 @@
-import axios from 'axios'
-var config = require('../../../config')
+import axios from "axios";
+import { log } from "util";
+var config = require("../../../config");
 
-var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+var frontendUrl = "http://" + config.dev.host + ":" + config.dev.port;
+var backendUrl =
+  "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
 
 var AXIOS = axios.create({
-    baseURL: backendUrl,
-    headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
+  baseURL: backendUrl,
+  headers: { "Access-Control-Allow-Origin": frontendUrl }
+});
 
 export default {
-    data() {
-        return {
-          students: [],
+  data() {
+    return {
+      students: [],
 
-          fields: {
-            firstName: {
-              label: 'FirstName',
-              sortable: true
-            },
-            lastName: {
-              label: 'LastName',
-              sortable: true
-            },
-            studentId:{
-              label: 'ID',
-              sortable: true
-            },
-            problematic:{
-              label: 'Problematic Status',
-              sortable: true
-            },
-            coopStatus :{
-              label: 'Coop Status',
-              sortable: true
-            },
-          }
-          
+      fields: {
+        firstName: {
+          label: "FirstName",
+          sortable: true
+        },
+        lastName: {
+          label: "LastName",
+          sortable: true
+        },
+        studentId: {
+          label: "ID",
+          sortable: true
+        },
+        problematic: {
+          label: "Problematic Status",
+          sortable: true
+        },
+        coopStatus: {
+          label: "Coop Status",
+          sortable:true
         }
-      },
+      }
+    };
+  },
 
-      created: function() {
-        // Initializing people from backend
-        AXIOS.get(`/allStudents`)
-          .then(response => {
-            this.students = response.data
-              })
-            
-            
-        for(var i=0 ; i< this.students.length; i++){
-          student = this.students[i];
-
-          student.coopStatus = student.coopPositions[0].status
-           }
-        }
+  created: function() {
     
+    // Initializing people from backend
+    AXIOS.get(`/allStudents`)
+      .then(response => {
+        this.students = response.data;
+      })
+      .then(() => {
+        for (var i = 0; i < this.students.length; i++) {
+          var student = this.students[i];
+          if (student.coopPositions.length > 0) {
+            student.coopStatus = student.coopPositions[0].status;
+          } else student.coopStatus = null;
+        }    
+            
+        this.$refs.table.refresh();
+      })
+      .catch(error => {
+        alert(error);
+      });
+
+      
   }
+};
