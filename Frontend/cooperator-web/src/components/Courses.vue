0ff61ad9ca452ac2d138743161b4ad94d81c4a30
Sophie Deng <sophie_deng97@hotmail.com>
2019-03-21 18:39:53 -0400

    #119 add the option to add courses

--- Frontend/cooperator-web/src/components/Courses.vue
+++ Frontend/cooperator-web/src/components/Courses.vue
@@ -3,23 +3,67 @@
     <div class="site-wrap">
         <div class="site-section" style="padding-top:10%">
             <div class="container" style="align:center">
-                <h1 class="page-title">Course Ranking</h1>
-                <div id="table">
-                    <table class="info-table" style="width: 100%; height: 100%;">
-                        <tr>
-                            <th style="padding:10px">Course Name</th>
-                            <th style="padding:10px">Number of Coops Used</th>
-                        </tr>
-                        <tr v-for="course in courses">
-                            <td style="padding:10px">{{ course.courseName }}</td>
-                            <td style="padding:10px">{{coopLength(course)}}</td>
-                        </tr>
-                    </table>
+                <h1 class="page-title">Create Course</h1>
+                <input
+                        class="text"
+                        type="text"
+                        placeholder="Course Name"
+                        v-model="newCourse"
+                        />
+                <input @click="createCourse(newCourse)"
+                        type="submit"
+                        value="Create"
+                        class="btn btn-primary py-2 px-4 text-white"
+                        />
+                <div class="container" align="center">
+                    <span v-if="errorNewCourse" style="color:red">Error: {{errorNewCourse}} </span>
+                    <br>
+                </div>
+                </div>
+                <div class="container" style="align:center">
+                    <h1 class="page-title">Add Course to Coop</h1>
+                    <input
+                        class="text"
+                        type="text"
+                        placeholder="CoopID"
+                        v-model="coopId"
+                        />
+                    <input
+                        class="text"
+                        type="text"
+                        placeholder="CourseID"
+                        v-model="courseId"
+                        />
+                    <input @click="rateCourse(coopId, courseId)"
+                        type="submit"
+                        value="Rate Course"
+                        class="btn btn-primary py-2 px-4 text-white"
+                        />
+                    <div class="container" align="center">
+                        <span v-if="errorCourse" style="color:red">Error: {{errorCourse}} </span>
+                        <br>
+                    </div>
+                    </div>
+                    <div class="container" style="align:center">
+                        <h1 class="page-title">Course Ranking</h1>
+                        <div id="table">
+                            <table class="info-table" style="width: 100%; height: 100%;">
+                                <tr>
+                                    <th style="padding:10px">Course ID</th>
+                                    <th style="padding:10px">Course Name</th>
+                                    <th style="padding:10px">Number of Coops Used</th>
+                                </tr>
+                                <tr v-for="course in courses">
+                                    <td style="padding:10px">{{ course.courseId }}</td>
+                                    <td style="padding:10px">{{ course.courseName }}</td>
+                                    <td style="padding:10px">{{coopLength(course)}}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 </template>
 
 <script src="./javascript/course.js">
