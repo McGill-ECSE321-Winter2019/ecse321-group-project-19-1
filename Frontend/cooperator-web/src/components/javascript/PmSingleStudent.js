cd2bcc03cd4d1fc24e073a75b09057b79ee5fb23
Max Brodeur-Urbas <murbas98@gmail.com>
2019-03-25 13:27:38 -0400

    changing the field of student table

--- Frontend/cooperator-web/src/components/javascript/PmSingleStudent.js
+++ Frontend/cooperator-web/src/components/javascript/PmSingleStudent.js
@@ -34,7 +34,7 @@ export default {
             sortable: true
           },
         submitted: {
-          label: "Submitted Status",
+          label: "Submitted?",
           sortable: true
         },
       }
