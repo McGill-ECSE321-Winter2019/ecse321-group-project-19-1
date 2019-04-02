6f03ee563218df36e07d2d027a3f3ffb329b3abb
Andre Kaba <andree.kaba@mail.mcgill.ca>
2019-04-02 02:11:55 -0400

    Initial android app

---
+++ Android/app/src/main/java/ecse321/mcgill/ca/cooperator/HttpUtils.java
@@ -0,0 +1,45 @@
+package ecse321.mcgill.ca.cooperator;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+public class HttpUtils {
+    //public static final String DEFAULT_BASE_URL = "http://cooperator-backend-260.herokuapp.com/";
+    public static final String DEFAULT_BASE_URL = "http://localhost:8080/";
+
+    private static String baseUrl;
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    static {
+        baseUrl = DEFAULT_BASE_URL;
+    }
+
+    public static String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public static void setBaseUrl(String baseUrl) {
+        HttpUtils.baseUrl = baseUrl;
+    }
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void getByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(url, params, responseHandler);
+    }
+
+    public static void postByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(url, params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return baseUrl + relativeUrl;
+    }
+}
